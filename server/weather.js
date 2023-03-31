dae183a36d81cb5e80a346fab196eedf63c50059
Dana <dabinkim807@gmail.com>
2023-03-30 20:57:23 -0400

    add hardcoded weather data

---
+++ server/weather.js
@@ -0,0 +1,47 @@
+const weatherAstoria = {
+  
+  "coord": {
+    "lon": -123.8313,
+    "lat": 46.1879
+  },
+  "weather": [
+    {
+      "id": 800,
+      "main": "Clear",
+      "description": "clear sky",
+      "icon": "01d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 280.23,
+    "feels_like": 278.46,
+    "temp_min": 277.92,
+    "temp_max": 281.56,
+    "pressure": 1021,
+    "humidity": 73
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 2.57,
+    "deg": 10
+  },
+  "clouds": {
+    "all": 0
+  },
+  "dt": 1677702821,
+  "sys": {
+    "type": 2,
+    "id": 2039875,
+    "country": "US",
+    "sunrise": 1677682490,
+    "sunset": 1677722453
+  },
+  "timezone": -28800,
+  "id": 5711847,
+  "name": "Astoria",
+  "cod": 200
+
+};
+
+module.exports = weatherAstoria;
